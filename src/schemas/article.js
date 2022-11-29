a595a6b3698f08dba3538249733d855afa444efe
GogumaYummy <sora1211x@gmail.com>
2022-11-29 20:13:23 +0900

    스키마 생성

---
+++ src/schemas/article.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const articlesSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  createdBy: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Articles', articlesSchema);
