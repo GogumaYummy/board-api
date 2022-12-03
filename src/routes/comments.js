542c55931cf1824b1f48fd97e605a9f2dd917d1e
GogumaYummy <sora1211x@gmail.com>
2022-12-03 19:54:13 +0900

    명세서 사양대로 변경

--- src/routes/comments.js
+++ src/routes/comments.js
@@ -10,17 +10,36 @@ router.post('/:postId', async (req, res, next) => {
     const { content } = req.body;
 
     if (!content || isNaN(postId))
-      throw new ApiError(400, '데이터 형식이 올바르지 않습니다.');
+      throw new ApiError(412, '데이터 형식이 올바르지 않습니다.');
 
     const post = await Post.findByPk(postId);
 
-    if (!post) throw new ApiError(404, '게시글 조회에 실패하였습니다.');
-
     await Comment.create({ content, postId: post.postId });
 
-    res.status(201).json({ message: '댓글을 생성하였습니다.' });
+    res.status(201).json({ message: '댓글을 작성하였습니다.' });
   } catch (err) {
-    next(err);
+    if (err instanceof ApiError) next(err);
+    else next(new ApiError(400, '댓글 작성에 실패하였습니다.'));
+  }
+});
+
+router.get('/:postId', async (req, res, next) => {
+  try {
+    const { postId } = req.params;
+
+    const comments = await Post.findByPk(postId, {
+      attributes: [],
+      include: {
+        model: Comment,
+        attributes: ['commentId', 'content', 'createdAt', 'updatedAt'],
+      },
+      order: [[Comment, 'commentId', 'DESC']],
+    });
+
+    res.status(201).json({ data: comments.Comments });
+  } catch (err) {
+    if (err instanceof ApiError) next(err);
+    else next(new ApiError(400, '댓글 조회에 실패하였습니다.'));
   }
 });
 
@@ -30,17 +49,20 @@ router.put('/:commentId', async (req, res, next) => {
     const { commentId } = req.params;
 
     if (!content || isNaN(commentId))
-      throw new ApiError(400, '데이터 형식이 올바르지 않습니다.');
+      throw new ApiError(412, '데이터 형식이 올바르지 않습니다.');
 
     const comment = await Comment.findByPk(commentId);
 
-    if (!comment) throw new ApiError(404, '댓글 조회에 실패하였습니다.');
+    if (!comment) throw new ApiError(404, '댓글이 존재하지 않습니다.');
 
     await comment.update({ content });
 
     res.status(200).json({ message: '댓글을 수정하였습니다.' });
   } catch (err) {
-    next(err);
+    if (err instanceof ApiError) next(err);
+    else if (err instanceof BaseError)
+      next(new ApiError(401, '댓글 수정이 정상적으로 처리되지 않았습니다.'));
+    else next(new ApiError(400, '댓글 수정에 실패하였습니다.'));
   }
 });
 
@@ -53,13 +75,16 @@ router.delete('/:commentId', async (req, res, next) => {
 
     const comment = await Comment.findByPk(commentId);
 
-    if (!comment) throw new ApiError(404, '댓글 조회에 실패하였습니다.');
+    if (!comment) throw new ApiError(404, '댓글이 존재하지 않습니다.');
 
     await comment.destroy();
 
     res.status(200).json({ message: '댓글을 삭제하였습니다.' });
   } catch (err) {
-    next(err);
+    if (err instanceof ApiError) next(err);
+    else if (err instanceof BaseError)
+      next(new ApiError(401, '댓글 삭제가 정상적으로 처리되지 않았습니다.'));
+    else next(new ApiError(400, '댓글 삭제에 실패하였습니다.'));
   }
 });
 
--- src/routes/posts.js
+++ src/routes/posts.js
@@ -1,51 +1,57 @@
 const { Router } = require('express');
 const ApiError = require('../utils/error');
-const { Post, Comment } = require('../db/models');
+const { Post } = require('../db/models');
+const { BaseError } = require('sequelize');
 
 const router = Router();
 
 router.post('/', async (req, res, next) => {
   try {
+    console.log(req.body);
     const { title, content } = req.body;
 
-    if (!title || !content)
-      throw new ApiError(400, '데이터 형식이 올바르지 않습니다.');
+    if (!title && !content)
+      throw new ApiError(412, '데이터 형식이 올바르지 않습니다.');
+    else {
+      if (!title)
+        throw new ApiError(412, '게시글 제목의 형식이 일치하지 않습니다.');
+      if (!content)
+        throw new ApiError(412, '게시글 내용의 형식이 일치하지 않습니다.');
+    }
 
     await Post.create({ title, content });
 
-    res.status(201).json({ message: '게시글을 생성하였습니다.' });
+    res.status(201).json({ message: '게시글 작성에 성공하였습니다.' });
   } catch (err) {
-    next(err);
+    if (err instanceof ApiError) next(err);
+    else next(new ApiError(400, '게시글 작성에 실패하였습니다.'));
   }
 });
 
-router.get('/', async (req, res) => {
-  const posts = await Post.findAll({
-    attributes: ['postId', 'title', 'createdAt', 'updatedAt'],
-  });
+router.get('/', async (req, res, next) => {
+  try {
+    const posts = await Post.findAll({
+      attributes: ['postId', 'title', 'createdAt', 'updatedAt'],
+      order: [['postId', 'DESC']],
+    });
 
-  res.status(200).json({ data: posts });
+    res.status(200).json({ data: posts });
+  } catch (err) {
+    if (err instanceof ApiError) next(err);
+    else next(new ApiError(400, '게시글 조회에 실패하였습니다.'));
+  }
 });
 
 router.get('/:postId', async (req, res, next) => {
   try {
     const { postId } = req.params;
 
-    if (isNaN(postId))
-      throw new ApiError(400, '데이터 형식이 올바르지 않습니다.');
-
-    const post = await Post.findByPk(postId, {
-      include: {
-        model: Comment,
-        attributes: ['commentId', 'content', 'createdAt', 'updatedAt'],
-      },
-    });
-
-    if (!post) throw new ApiError(404, '게시글 조회에 실패하였습니다.');
+    const post = await Post.findByPk(postId);
 
     res.status(200).json({ data: post });
   } catch (err) {
-    next(err);
+    if (err instanceof ApiError) next(err);
+    else next(new ApiError(400, '게시글 조회에 실패하였습니다.'));
   }
 });
 
@@ -54,18 +60,25 @@ router.put('/:postId', async (req, res, next) => {
     const { title, content } = req.body;
     const { postId } = req.params;
 
-    if (!title || !content || isNaN(postId))
-      throw new ApiError(400, '데이터 형식이 올바르지 않습니다.');
+    if ((!title && !content) || isNaN(postId))
+      throw new ApiError(412, '데이터 형식이 올바르지 않습니다.');
+    else {
+      if (!title)
+        throw new ApiError(412, '게시글 제목의 형식이 일치하지 않습니다.');
+      if (!content)
+        throw new ApiError(412, '게시글 내용의 형식이 일치하지 않습니다.');
+    }
 
     const post = await Post.findByPk(postId);
-
-    if (!post) throw new ApiError(404, '게시글 조회에 실패하였습니다.');
 
     await post.update({ title, content });
 
     res.status(200).json({ message: '게시글을 수정하였습니다.' });
   } catch (err) {
-    next(err);
+    if (err instanceof ApiError) next(err);
+    else if (err instanceof BaseError)
+      next(new ApiError(401, '게시글이 정상적으로 수정되지 않았습니다.'));
+    else next(new ApiError(400, '게시글 수정에 실패하였습니다.'));
   }
 });
 
@@ -73,18 +86,18 @@ router.delete('/:postId', async (req, res, next) => {
   try {
     const { postId } = req.params;
 
-    if (isNaN(postId))
-      throw new ApiError(400, '데이터 형식이 올바르지 않습니다.');
-
     const post = await Post.findByPk(postId);
 
-    if (!post) throw new ApiError(404, '게시글 조회에 실패하였습니다.');
+    if (!post) throw new ApiError(404, '게시글이 존재하지 않습니다.');
 
     await Post.destroy({ where: { postId } });
 
     res.status(200).json({ message: '게시글을 삭제하였습니다.' });
   } catch (err) {
-    next(err);
+    if (err instanceof ApiError) next(err);
+    else if (err instanceof BaseError)
+      next(new ApiError(401, '게시글이 정상적으로 삭제되지 않았습니다.'));
+    else next(new ApiError(400, '게시글 작성에 실패하였습니다.'));
   }
 });
 
