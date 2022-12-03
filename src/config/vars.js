f2c75b93221a56d2ae5b3ef211c722c86cf3cc9e
GogumaYummy <sora1211x@gmail.com>
2022-12-03 18:41:05 +0900

    MySQL로 마이그레이션

--- src/config/vars.js
+++ src/config/vars.js
@@ -12,5 +12,5 @@ module.exports = {
   dbUser: process.env.DB_USER,
   dbPassword: process.env.DB_PASSWORD,
   dbHost: process.env.DB_HOST,
-  dbName: process.env.DB_NAME,
+  dbDatabase: process.env.DB_DATABASE,
 };
